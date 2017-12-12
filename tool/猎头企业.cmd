6d30588a09f2ef4db2fd9cbbac7b1a012b103f80
Charlie <2531763117@qq.com>
2017-12-12 14:35:30 +0800

    Update 猎头企业.cmd

--- tool/猎头企业.cmd
+++ tool/猎头企业.cmd
@@ -9,3 +9,68 @@ Manpower,
 Mango,
 雷诺士，
 安立国际，
+
+世界十大顶尖猎头公司排行榜（排名分先后）： 
+1. 光辉国际咨询顾问公司  Korn/Ferry International  
+2. 海德思哲国际有限公司  Heidrick & Struggles  
+3. 美国阿托兹顾问有限公司  ATOZ Consultants  
+4. 亿康先达国际咨询公司   Egon Zehnder International  
+5. 罗兰贝格尔国际有限公司  Roland Berger International 
+6. 科尔尼国际有限公司  A.T. Kearney Executive Search  
+7. 优异人力资源顾问公司  Sterling Human Resource Consulting  
+8. EMDS顾问有限公司  EMDS consulting  
+9. 联合资源顾问有限公司   United Resource Networks  
+10.优利投资咨询有限公司  Uniland Development
+
+
+世界猎头（人力资源咨询服务）公司前50名公司排行榜（排名分先后）： 
+ 1.  光辉国际     Korn/Ferry International 
+2.  万宝盛华    Manpower   
+3.  海德思哲     Heidrick & Struggles  
+ 4.  瑞士阿第克人力资源公司 Adecco Group 
+5.  海士国际咨询  HAX INTERNATIONAL   
+6.  科尔尼高级咨询  A.T. Kearney Executive Search  
+7.  安立国际咨询服务    Amrop Hever Group ＩInternational 
+8.  罗兰贝格尔国际咨询   Roland Berger International  
+9.  雷诺仕国际咨询公司  Russell Reynolds Associates New York  
+10. 尼科尔森国际有限公司  Nicholson Internationallt 
+11. 亿康先达国际咨询公司  Egon Zehnder International  
+12. 雷奔逊人才搜寻公司  Ray & Berndtson Fort Worth  
+13. 浩华国际有限公司  Ward Howell International   
+14. 豪敦咨询有限公司 Horton International  
+15. 尤里克咨询有限公司  unique-china Consultants 
+16. 斯坦顿大通国际咨询公司 StantonChase International 
+17. 雷文顾问管理公司  Noman Broadbent
+18. 普群国际咨询   PCI –IIC Partners  
+19. 德勤咨询  Deloitte Touche Consulting  
+20. 翰威特咨询  Hewitt Consulting Consultants 
+21. 阿托兹顾问  ATOZ Consultants  
+22. 优异人力资源咨询  Sterling Human Resource Consulting
+23. 杰迈晶雅人力资源公司  J. M. Gemini Personal Ltd. 
+24. 丹尼尔森人才顾问公司  Danielson Consulting Co.,Ltd. 
+25. 安拓国际顾问公司  Antal International 
+26. 优瑞集团  Euro Group International 
+27. 波士顿咨询 Boston Consulting Group  
+28. 讯升咨询  comprise Consultants 
+29. 天普国际  Templar International  
+30. 摩根柏客顾问公司  Morgan & Banks Resources Ltd
+31. 哈德森环球资源  TMP/Hudson Global Resource  
+32. 福斯特合伙人  Foster Partners Executive   
+33. MMC      Marakon Management Consultants 
+34. 克伦博国际顾问  Kienbaum Consultants International 
+35. 翰德国际顾问  Hudson Global Resources Ltd  
+36.  KSA          Kurt Salmon Associates  
+37. 人合国际管理顾问  MRI Worldwide Consultants 
+38. 贝拉特管理顾问有限公司  Bilast Management Consultants 
+39. 史宾沙咨询  Spencer Stuart Consultants  
+40. 德鑫管理咨询  Fiducia Management Consultants  
+41. GrammyTech     GrammyTech Ltd  
+42. P -无限咨询   P-Infinity Consultants  
+43. SRI          SRI Consulting Group 
+44. 欧信国际咨询服务  China Team International   
+45. 索邦管理咨询公司  SearchBank Management Consultants 
+46. 伯乐管理公司  Bó Lè Associates Ltd 
+47. 宝鼎国际咨询  Boyden International Ltd 
+48. 亚通咨询  Executive Associates   
+49. BTG           Brocker Technology Group  
+50. 机构发展顾问公司  Organisation Search Ltd 
